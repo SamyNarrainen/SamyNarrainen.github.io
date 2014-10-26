b5c73e609d87f20fe344dd244532c800875b0ec5
Samy Narrainen <samynarrainen@gmail.com>
2014-10-26 16:57:32 +0000

    Will JQuery work?
    moved jquery to main.js and changed srcs to // not https://

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 
+    		$(function() {
+    		    $("#navigationBarList li a").hover(
+    		        function() {
+    		            $(this).animate({ color: "#2777EC" }, 'slow');
+    		        },function() {
+    		            $(this).animate({ color: "#FFFFFF" }, 'slow');
+    		    });
+    		});
+
+    		$(function() {
+   				window.scrollTo(0,0);
+			});
+
+		$(document).ready(function(){
+			$('a[href^="#"]').on('click',function (e) {
+	   			e.preventDefault();
+
+	    		var target = this.hash;
+	   			$target = $(target);
+
+	    		$('html, body').stop().animate({
+	        		'scrollTop': $target.offset({}).top
+	    		}, 900, 'swing', function () {
+	        		window.location.hash = target;
+	    		});
+
+	    		$('html, body').stop().animate({
+     				'scrollTop': $target.offset().top
+				}, 900, 'swing');
+
+
+
+			});
+		});
